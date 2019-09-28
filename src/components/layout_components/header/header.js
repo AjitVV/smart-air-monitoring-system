98241028e0715843794095d3b430dd995f401d04
VV, AJIT <AJIT.VV@cerner.com>
2019-09-28 11:33:56 +0530

    added page header

---
+++ src/components/layout_components/header/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Header() {
+    return (
+        <nav class="navbar navbar-inverse">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" >Smart-Air Monitoring System</a>
+                </div>
+            </div>
+        </nav>
+    )
+}
